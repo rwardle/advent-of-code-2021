1c83361e9cd6d58adea4ccace6289780baaf452d
Richard Wardle <471494+rwardle@users.noreply.github.com>
2021-12-12 17:17:09 +0000

    Day 7, part 2

--- src/com/richardwardle/aoc2021/day7/Day7.java
+++ src/com/richardwardle/aoc2021/day7/Day7.java
@@ -2,12 +2,31 @@ package com.richardwardle.aoc2021.day7;
 
 import com.richardwardle.aoc2021.input.InputUtils;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
 
 public class Day7 {
 
     public static void main(String[] args) {
         new Day7().execute();
+    }
+
+    private static String readInput() {
+        try (var scanner = InputUtils.day7Data()) {
+            if (scanner.hasNext()) {
+                return scanner.nextLine();
+            }
+        }
+
+        throw new RuntimeException("Invalid data");
+    }
+
+    private static int calculateFuel(int crabPosition, int targetPosition) {
+        int distance = Math.abs(crabPosition - targetPosition);
+        return IntStream.rangeClosed(1, distance).sum();
     }
 
     private void execute() {
@@ -22,7 +41,7 @@ public class Day7 {
             var targetPosition = i;
             var fuel = crabPositions
                     .stream()
-                    .map(c -> Math.abs(c - targetPosition))
+                    .map(c -> calculateFuel(c, targetPosition))
                     .mapToLong(s -> s)
                     .sum();
             fuelByPosition.put(targetPosition, fuel);
@@ -33,15 +52,5 @@ public class Day7 {
                 .min(Map.Entry.comparingByValue())
                 .orElseThrow();
         System.out.printf("Position: %d, fuel: %d", entry.getKey(), entry.getValue());
-    }
-
-    private String readInput() {
-        try (Scanner scanner = InputUtils.day7Data()) {
-            if (scanner.hasNext()) {
-                return scanner.nextLine();
-            }
-        }
-
-        throw new RuntimeException("Invalid data");
     }
 }
